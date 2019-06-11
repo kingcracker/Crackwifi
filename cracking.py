057d297b85d328b1b635cac45ae26de886d25ddc
MR.51MB4H <48198896+kingcracker@users.noreply.github.com>
2019-06-11 08:42:45 +0700

    Update cracking.py

--- cracking.py
+++ cracking.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import time
 
 print ("╔═╗╔═╗╔══╗╔═╗╔╦╗")
 print ("║╔╝║╬║║╔╗║║╔╝║╔╝")
@@ -15,93 +15,33 @@ print("Create by: MR.51MB4H")
 print("hack wifi: ")
 print("______waiting_________")
 
-
-
-
-
-
+for i in range(10, 0, -1):
+    time.sleep(2)
+    print str(i)
 
 input ("nama wifi: ")
 print ("")
-
-
-
-
-
-
-
-
-
-
-
+time.sleep(3)
 print("sedang mencari info")
-
 print("")
-
-
-
-
+time.sleep(2)
 print("mengizinkan setup wifi")
 print("")
-
-
-
-
-
-
+time.sleep(2)
 print("[!] Berhasil Mendapat   Wifi")
-
-
-
-
+time.sleep(3)
 print("Information Wifi")
 print("")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+time.sleep(2)
 print("Ip hiAddress : 192.56.77.2")
+time.sleep(2)
 print("Password : modal cuk")
+time.sleep(1)
 print("Wifi Security : WPA/WP2 PSK")
+time.sleep(3)
 print("_/\/\/\/\/\________dead...")
-
 print("")
-
-
-
-
-
-
-
-
-
-
-
+time.sleep(2)
 print("goblok lu")  
 
 print("thanks sudah pakai tools ini")
